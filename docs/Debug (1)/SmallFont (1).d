aa9781d535ac655d28dc1a362c9f1db9f14f1aef
Nirmay Das <nirmaydas99@gmail.com>
2025-05-21 17:57:37 -0500

    Organized project into folders and cleaned up file structure

---
+++ docs/Debug (1)/SmallFont (1).d
@@ -0,0 +1,9 @@
+# FIXED
+
+SmallFont.o: ../SmallFont.c \
+ C:/Users/nolan/Downloads/MSPM0_ValvanoWare/MSPM0_ValvanoWare/ECE319K_Lab9/SmallFont.h \
+ C:/Users/nolan/Downloads/MSPM0_ValvanoWare/MSPM0_ValvanoWare/inc/ST7735.h
+
+C:/Users/nolan/Downloads/MSPM0_ValvanoWare/MSPM0_ValvanoWare/ECE319K_Lab9/SmallFont.h:
+
+C:/Users/nolan/Downloads/MSPM0_ValvanoWare/MSPM0_ValvanoWare/inc/ST7735.h:
---
+++ docs/Debug (1)/images (1)/pipedreams (1)/subdir_rules (1).mk
@@ -0,0 +1,15 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Each subdirectory must supply rules for building sources it contributes
+images/pipedreams/%.o: ../images/pipedreams/%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: Arm Compiler'
+	"C:/ti/ccs1270/ccs/tools/compiler/ti-cgt-armllvm_3.2.2.LTS/bin/tiarmclang.exe" -c -march=thumbv6m -mcpu=cortex-m0plus -mfloat-abi=soft -mlittle-endian -mthumb -O0 -I"C:/Users/nirma/OneDrive/Desktop/319K/New_MSPM0_ValvanoWare/New_MSPM0_ValvanoWare/ECE319K_Lab9" -I"C:/Users/nirma/OneDrive/Desktop/319K/New_MSPM0_ValvanoWare/New_MSPM0_ValvanoWare/ECE319K_Lab9/Debug" -I"C:/ti/mspm0_sdk_2_00_00_03/source/third_party/CMSIS/Core/Include" -I"C:/ti/mspm0_sdk_2_00_00_03/source" -D__MSPM0G3507__ -gdwarf-3 -MMD -MP -MF"images/pipedreams/$(basename $(<F)).d_raw" -MT"$(@)" -I"C:/Users/nirma/OneDrive/Desktop/319K/New_MSPM0_ValvanoWare/New_MSPM0_ValvanoWare/ECE319K_Lab9/Debug/syscfg"  $(GEN_OPTS__FLAG) -o"$@" "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+
